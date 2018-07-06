18d71050b773bbde7982e71a81f6d6a785a0a6a0
rsrdesarrollo <rsrdesarrollo@gmail.com>
2018-07-06 07:41:18 -0400

    add wait for db on entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,13 @@ fi
 
 chown sarna:sarna /sarna/uploaded_data /sarna/static
 
+DATABASE_HOST=$(echo $SQLALCHEMY_DATABASE_URI | sed -r 's#\w+://[^:]+:[^@]+@([^/]+)/.*#\1#')
+until nc -z -v -w30 $DATABASE_HOST 5432
+do
+  echo "Waiting for database connection..."
+  # wait for 5 seconds before check again
+  sleep 5
+done
+
 su -c 'flask db upgrade' sarna
 su -c 'gunicorn -w 4 -b 0.0.0.0:5000 --access-logfile - --error-logfile - app:app' sarna
