b219d2ac146dd7fc298ddbb18de5e918b7c3dc4e
fillodemanuel <63927071+fillodemanuel@users.noreply.github.com>
2020-10-01 17:48:48 +0200

    Removed OWASP Categories (OTG)

--- sarna/model/enums/__init__.py
+++ sarna/model/enums/__init__.py
@@ -1,5 +1,5 @@
 from .assessment import AssessmentStatus, AssessmentType
-from .category import OWISAMCategory, OWASPCategory, WSTG
+from .category import OWISAMCategory, WSTG
 from .finding import FindingStatus, FindingType
 from .language import Language
 from .report import SequenceName
@@ -8,6 +8,6 @@ from .user import UserType, AuthSource
 from .analysis import AnalysisResultType
 
 __all__ = [
-    'UserType', 'AuthSource', 'AssessmentStatus', 'AssessmentType', 'OWISAMCategory', 'OWASPCategory',
+    'UserType', 'AuthSource', 'AssessmentStatus', 'AssessmentType', 'OWISAMCategory',
     'FindingStatus', 'FindingType', 'Language', 'Score', 'SequenceName', 'AnalysisResultType', 'WSTG'
 ]
